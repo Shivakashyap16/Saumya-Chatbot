4b2f3fcff819967f3ff9f36156fbff7f213b4f98
Shiva Kashyap Yellavajhala <138601725+Shivakashyap16@users.noreply.github.com>
2025-01-05 11:35:40 +0530

    Update app.py

--- Saumya-Chatbot/app.py
+++ Saumya-Chatbot/app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 CORS(app)  # Enable CORS
 
 # Configure the API key
-API_KEY = 'Paste you API Key here'
+API_KEY = 'Paste your API Key here'
 genai.configure(api_key=API_KEY)
 
 # Initialize the chat model
